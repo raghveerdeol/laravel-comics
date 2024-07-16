53cf6e41d649fa689fa6b32aa9fc3152b12c623e
raghveerdeol <deolraghveer5@gmail.com>
2024-07-16 22:37:33 +0200

    aggiunto footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,1 +1,61 @@
-<h1>sono footer</h1>
+<footer id="app-footer">
+    <section>
+        <div class="footer-link">
+            <div class="links">
+                <div>
+                    <h2>
+                        dc comics
+                    </h2>
+                    <ul class="dc-comics">
+                        @foreach ($dcComixLinks as $link)
+                        <li>
+                            {{ $link['title'] }}
+                        </li>
+                        @endforeach
+                    </ul>
+                </div>
+                <div>
+                    <h2>
+                        shop
+                    </h2>
+                    <ul class="shop">
+                        @foreach ($shopLinks as $link)
+                        <li>
+                            {{ $link['title'] }}
+                        </li>
+                        @endforeach
+                    </ul>
+                </div>
+                <div>
+                    <h2>
+                        dc
+                    </h2>
+                    <ul class="dc">
+                        @foreach ($dcLinks as $link )
+                        <li>
+                            {{ $link['title'] }}
+                        </li>
+                        @endforeach
+                    </ul>
+                </div>
+                <div>
+                    <h2>
+                        sites
+                    </h2>
+                    <ul class="sites">
+                        @foreach ($sitesLinks as $link)
+                        <li>
+                            {{ $link['title'] }}
+                        </li>
+                        @endforeach
+                    </ul>
+                </div>
+            </div>
+
+            <div class="logo">
+                <img src="../../img/dc-logo-bg.png" alt="logo">
+            </div>
+        </div>
+        <AppSignUp/>
+    </section>
+</footer>
